fab4b58992686db34d936c45b4a7b17070aaa7ad
SumayyahSayyed <89860998+SumayyahSayyed@users.noreply.github.com>
2023-07-05 17:17:50 +0500

    Update app.js

--- app.js
+++ app.js
@@ -25,113 +25,79 @@ let label = document.querySelector(".label");
 let results = document.querySelector(".results")
 
 
-function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
-}
-
-let tossWinner = getRandomInt(2);
-tossWinner += 1;
-
 let buttons = document.querySelectorAll('.container-child');
 
+function hover() {
+    if (turn === "user1") {
+        blocks.forEach(block => {
+            block.classList.add("hover-blue");
+            block.classList.remove("hover-red");
+        })
+    }
+    if (turn === "user2") {
+        blocks.forEach(block => {
+            block.classList.add("hover-red");
+            block.classList.remove("hover-blue");
+        })
+    }
+    blocks.forEach(block => {
+        if (block.hasChildNodes()) {
+            block.classList.remove('hover-blue');
+            block.classList.remove('hover-red');
+        }
+    });
+}
 
 if (isGameOver === false) {
-    console.log("Game over value in if statement: ", isGameOver);
-    if (tossWinner === 1) {
-        turn = 'user1';
+    turn = 'user1';
+    user1.style.color = "#2b29a3";
+    user2.style.color = "#b5171f";
 
-        user1.style.color = "#2b29a3";
-        user2.style.color = "#b5171f";
+    blocks.forEach((block, index) => {
+        block.classList.add('hover-blue');
 
-        blocks.forEach((block, index) => {
+        block.addEventListener("click", () => {
 
-            block.addEventListener("click", () => {
+            if (turn === 'user1') {
+                let tickMark = document.createElement("img");
+                tickMark.classList.add("image-class");
+                tickMark.src = "images/Tick-mark.png";
+                block.appendChild(tickMark);
 
-                if (turn === 'user1') {
-                    let tickMark = document.createElement("img");
-                    tickMark.classList.add("image-class");
-                    tickMark.src = "images/Tick-mark.png";
-                    block.appendChild(tickMark);
+                block.disabled = true;
 
-                    block.disabled = true;
-                    turn = 'user2';
-                }
+                turn = 'user2';
+                hover();
+            }
 
-                else if (turn === 'user2') {
-                    let crossMark = document.createElement("img");
-                    crossMark.classList.add("image-class");
-                    crossMark.src = "images/cross-mark.png";
-                    block.appendChild(crossMark);
+            else if (turn === 'user2') {
+                let crossMark = document.createElement("img");
+                crossMark.classList.add("image-class");
+                crossMark.src = "images/cross-mark.png";
+                block.appendChild(crossMark);
 
-                    block.disabled = true;
-                    turn = 'user1';
-                }
+                block.disabled = true;
 
-                let imageChild = block.querySelectorAll(".image-class");
+                turn = 'user1';
+                hover();
+            }
 
-                if (imageChild.length > 0) {
-                    imageChild.forEach(image => {
-                        src = image.getAttribute("src");
-                        options[index] = src;
-                    })
-                }
-                else {
-                    options.push(" ");
-                }
+            let imageChild = block.querySelectorAll(".image-class");
 
-                checkWinner();
-            })
+            if (imageChild.length > 0) {
+                imageChild.forEach(image => {
+                    src = image.getAttribute("src");
+                    options[index] = src;
+                })
+            }
+            else {
+                options.push(" ");
+            }
 
+            checkWinner();
         })
 
-    }
-
-    if (tossWinner === 2) {
-        turn = 'user2';
-
-        user2.style.color = "#2b29a3";
-        user1.style.color = "#b5171f";
-
-        blocks.forEach((block, index) => {
-
-            block.addEventListener("click", () => {
-
-                if (turn === 'user2') {
-                    let crossMark = document.createElement("img");
-                    crossMark.classList.add("image-class");
-                    crossMark.src = "images/cross-mark.png";
-                    block.appendChild(crossMark);
-
-                    block.disabled = true;
-                    turn = 'user1';
-                }
-                else if (turn === 'user1') {
-                    let tickMark = document.createElement("img");
-                    tickMark.classList.add("image-class");
-                    tickMark.src = "images/Tick-mark.png";
-                    block.appendChild(tickMark);
-
-                    block.disabled = true;
-                    turn = 'user2';
-
-                }
-                let imageChild = block.querySelectorAll(".image-class");
-
-                if (imageChild.length > 0) {
-                    imageChild.forEach(image => {
-                        src = image.getAttribute("src");
-                        options[index] = src;
-                    })
-                }
-                else {
-                    options.push(" ");
-                }
-                areAllBlocksFilled();
-                checkWinner();
-            })
-
-        })
-    }
+    })
 }
 
 function areAllBlocksFilled() {
@@ -145,23 +111,15 @@ function areAllBlocksFilled() {
 }
 
 function checkWinner() {
+    let hasWinner = false;
 
     for (let k = 0; k < combinations.length; k++) {
         let singleCombo = combinations[k];
-
         let cellA = options[singleCombo[0]];
         let cellB = options[singleCombo[1]];
         let cellC = options[singleCombo[2]];
 
-        if (
-            typeof cellA === "undefined"
-            || cellA === "" || typeof cellB === "undefined"
-            || cellB === "" || typeof cellC === "undefined"
-            || cellC === ""
-        ) {
-            continue;
-        }
-        else if (cellA === cellB && cellB === cellC && cellA !== "" && cellB !== "" && cellC !== "") {
+        if (cellA && cellA === cellB && cellB === cellC) {
             console.log("game won");
             console.log(cellA, cellB, cellC);
 
@@ -169,23 +127,20 @@ function checkWinner() {
 
             if (cellA === "images/Tick-mark.png") {
                 label.textContent = "USER1 WON";
-            }
-            if (cellA === "images/cross-mark.png") {
+            } else if (cellA === "images/cross-mark.png") {
                 label.textContent = "USER2 WON";
             }
 
             GameOver();
+            hasWinner = true;
             break;
         }
-        else if (areAllBlocksFilled()) {
-            results.classList.remove("hide");
-            label.textContent = "DRAW";
+    }
 
-            GameOver();
-        }
-        else {
-            continue;
-        }
+    if (!hasWinner && areAllBlocksFilled()) {
+        results.classList.remove("hide");
+        label.textContent = "DRAW";
+        GameOver();
     }
 }
 
@@ -200,11 +155,13 @@ function GameOver() {
 
 reset.addEventListener("click", () => {
     results.classList.add("hide");
+
     blocks.forEach(block => {
         while (block.firstChild) {
             block.removeChild(block.firstChild);
             isGameOver = false;
         }
+        hover();
         block.disabled = false;
         label.textContent = "";
         console.log("Removed images");
